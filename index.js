50796f21b4979efbe8daa580b09522ca28265426
Charles Jones <charlie.l.jones@gmail.com>
2025-02-28 09:54:49 -0500

    fix: MongoDB connection

--- index.js
+++ index.js
@@ -14,13 +14,22 @@ const {
   DB_NAME,
 } = process.env;
 
-const MONGO_URI = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?authSource=admin`
+const MONGO_URI = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/?retryWrites=true&w=majority&appName=MERN`
 
-// Connect DB
-mongoose
-  .connect(MONGO_URI,{ useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log("mongoDB is connected"))
-  .catch((err) => console.log(err));
+async function connectDatabase() {
+  try {
+    // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
+    await mongoose.connect(MONGO_URI, { serverApi: { version: '1', strict: true, deprecationErrors: true } });
+    await mongoose.connection.db.admin().command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await mongoose.disconnect();
+  }
+}
+
+// Connect to MongoDB
+connectDatabase().catch(console.dir);
 
 // Middleware
 app.use(express.json());
